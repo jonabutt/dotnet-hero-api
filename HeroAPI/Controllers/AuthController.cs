d0e83a5c1229a042d9acf5d6b754ad337ef8820b
jonabutt <jonathanbuttigieg1@gmail.com>
2022-05-29 11:18:35 +0200

    Register method in auth controller

---
+++ HeroAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using HeroAPI.Helpers.Auth;
+using HeroAPI.Models;
+using HeroAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<HeroController> _logger;
+        public AuthController(DataContext context, ILogger<HeroController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        // POST: api/v1/Hero/register
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterViewModel registerDetails)
+        {
+            // TODO: validation using DataAnnotations
+            PasswordHelper.CreatePasswordHash(registerDetails.Password,out byte[] salt,out byte[] passwordHash);
+            await _context.Users.AddAsync(new User()
+            {
+                CreatedAt = DateTime.UtcNow,
+                PasswordHash = salt,
+                PasswordSalt = passwordHash,
+                Username = registerDetails.Username
+            });
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ HeroAPI/Helpers/Auth/PasswordHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HeroAPI.Helpers.Auth
+{
+    public static class PasswordHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] salt, out byte[] passwordHash)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                salt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
